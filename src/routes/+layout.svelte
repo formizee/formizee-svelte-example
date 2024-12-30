fd90cf2c14e96479f115c6b0b6e9878f84db6208
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-30 18:54:28 +0100

    feat: create the example

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+	import svelteLogo from '$lib/assets/svelte.svg';
+	import windowIcon from '$lib/assets/window.svg';
+	import globeIcon from '$lib/assets/globe.svg';
+	import fileIcon from '$lib/assets/file.svg';
+
+	let { children } = $props();
+</script>
+
+<header>
+  <h1>Formizee.</h1>
+  <span>+</span>
+  <figure>
+    <img src={svelteLogo}
+      alt="Svelte logo"
+      height="38"
+      width="38"
+    />
+    <span>Svelte</span>
+  </figure>
+</header>
+<main>
+{@render children()}
+</main>
+<footer>
+  <div>
+    <a
+      href="https://docs.formizee.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        aria-hidden={true}
+        src={fileIcon}
+        alt="File icon"
+        width="16"
+        height="16"
+      />
+      Learn
+    </a>
+    <a
+      href="https://docs.formizee.com/docs/quickstart"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        aria-hidden={true}
+        src={windowIcon}
+        alt="Window icon"
+        width="16"
+        height="16"
+      />
+      Examples
+    </a>
+    <a
+      href="https://formizee.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        aria-hidden={true}
+        src={globeIcon}
+        alt="Globe icon"
+        width="16"
+        height="16"
+      />
+      formizee.com
+    </a>
+  </div>
+</footer>
+
+<style>
+	:global(body) {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    min-height: 100vh;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    text-rendering: optimizeLegibility;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+	}
+
+  main {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  header {
+    gap: 1rem;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    h1, span {
+      font-weight: 700;
+    }
+
+    figure {
+      flex-direction: row;
+      align-items: center;
+      display: flex;
+      gap: 10px;
+
+      margin: 0;
+      font-size: 25px;
+      font-weight: 500;
+    }
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 2rem;
+    padding: 8px;
+
+    div {
+      display: flex;
+      flex-direction: row;
+      gap: 2rem;
+    }
+
+    a {
+      gap: 1rem;
+      color: black;
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      font-family: 'Space Grotesk'
+    }
+    a:hover {
+      text-decoration: underline;
+      text-underline-offset: 4px;
+    }
+  }
+
+
+  @font-face {
+    font-family: 'Inter';
+    src: url('/fonts/inter.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Space Grotesk';
+    src: url('/fonts/space-grotesk.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<form method="post" action="https://api.formizee.com/v1/f/enp_123456">
+  <label htmlFor="name">
+    Name
+    <input id="name" autoComplete="name" name="name" />
+  </label>
+  <label htmlFor="email">
+    Email
+    <input id="email" autoComplete="email" name="email" />
+  </label>
+  <button type="submit">Submit</button>
+</form>
+
+<style>
+  form {
+    gap: 2rem;
+    width: 100%;
+    padding: 8px;
+    display: flex;
+    max-width: 420px;
+    margin-top: 2rem;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+
+    label {
+      gap: 0.5rem;
+      width: 100%;
+      display: flex;
+      color: #525252;
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+      flex-direction: column;
+    }
+
+    input {
+      display: flex;
+      height: 2.25rem;
+      border-radius: 0.365rem;
+
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+
+      background: transparent;
+
+      border: 1px solid #e5e5e5;
+      box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+
+      transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      transition-duration: 150ms;
+    }
+
+    input:focus-visible {
+      outline: 2px solid transparent;
+      outline-offset: 2px;
+      box-shadow: inset 0 0 0 2px #d4d4d4;
+    }
+
+    input::placeholder {
+      color: #a3a3a3;
+    }
+
+    button {
+      width: 100%;
+      display: flex;
+      height: 2.5rem;
+      align-items: center;
+      justify-content: center;
+
+      border-width: 2px;
+      border-color: #404040;
+      border-radius: 0.365rem;
+
+      color: #fafafa;
+      background-color: #171717;
+
+      font-weight: 600;
+      font-family: "Space Grotesk";
+
+      cursor: pointer;
+      transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      transition-duration: 150ms;
+    }
+
+    button:hover {
+      background-color: #262626;
+      border-color: #737373;
+    }
+  }
+</style>
